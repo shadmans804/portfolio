642fb19ce533ec707bc5b59cdd147481332d717f
unknown <shadmans804@gmail.com>
2024-06-25 03:39:10 +0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import Education from './Education';
+import UnderConstruction from './UnderConstruction';
+
+
+export { Home, About, Projects, Contact, Education, UnderConstruction };
